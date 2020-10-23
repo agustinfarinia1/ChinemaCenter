44c3fc35027aef04eb03bcf87c1d465b6b3a4156
Jose Alvarez W <jsemalvarez@gmail.com>
2020-10-22 21:51:31 -0300

    add gitignore

--- Config/ExampleConfig.php
+++ Config/ExampleConfig.php
@@ -3,15 +3,20 @@
 define("ROOT", dirname(__DIR__) . "/");
 
 //Path to your project's root folder
-define("FRONT_ROOT", "/TP FINAL 2020/chinemacenter/");
-
+define("FRONT_ROOT", "/Chinemacenter/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "layout/styles/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
 define("IMG_PATH", FRONT_ROOT.VIEWS_PATH . "img/");
 
-//Personal's config
-define("API_KEY", "7e5de46aba8f155b486beee9b4b4cc4f");
+//Api's config
+define("API_KEY", "");
+
+//DB's config
+define("DB_HOST", "localhost");
+define("DB_NAME", "");
+define("DB_USER", "");
+define("DB_PASS", "");
 
 ?>
 

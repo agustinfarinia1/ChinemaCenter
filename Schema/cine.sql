f0b866f44902a68d4059e327059acbe43bafb553
Jose Alvarez W <jsemalvarez@gmail.com>
2020-11-12 10:25:51 -0300

    tabla de peliculas

--- Schema/cine.sql
+++ Schema/cine.sql
@@ -77,7 +77,69 @@ DROP procedure IF EXISTS `SP_FUN_GET_ALL`;
 DELIMITER $$
 CREATE PROCEDURE SP_FUN_GET_ALL()
 BEGIN
-	SELECT * FROM funciones WHERE estado = 1 ORDER BY id_funcion DESC;
+	SELECT
+      f.id_funcion,
+	  f.id_sala,
+	  f.id_pelicula,
+	  f.fecha_inicio,
+	  f.fecha_fin,
+	  f.hora_inicio,
+	  f.hora_fin,
+	  f.lunes,
+	  f.martes,
+	  f.miercoles,
+	  f.jueves,
+	  f.viernes,
+	  f.sabado,
+	  f.domingo,
+      f.estreno,
+      c.nombre AS nombre_cine,
+      s.nombre AS nombre_sala,     
+      p.nombre AS nombre_pelicula,
+      p.poster,
+      p.foto,
+      p.comentario
+    FROM funciones f
+    INNER JOIN salas s ON s.id_sala = f.id_sala
+    INNER JOIN peliculas p ON p.id_pelicula = f.id_pelicula
+    INNER JOIN cines c ON c.id_cine = s.id_cine
+    WHERE f.estado = 1 ORDER BY f.id_funcion DESC;
+END$$
+DELIMITER ;
+
+
+
+DROP procedure IF EXISTS `SP_FUN_GET_X_ID`;
+DELIMITER $$
+CREATE PROCEDURE SP_FUN_GET_X_ID(IN id INT)
+BEGIN
+	SELECT
+      f.id_funcion,
+	  f.id_sala,
+	  f.id_pelicula,
+	  f.fecha_inicio,
+	  f.fecha_fin,
+	  f.hora_inicio,
+	  f.hora_fin,
+	  f.lunes,
+	  f.martes,
+	  f.miercoles,
+	  f.jueves,
+	  f.viernes,
+	  f.sabado,
+	  f.domingo,
+      f.estreno,
+      c.nombre AS nombre_cine,
+      s.nombre AS nombre_sala,     
+      p.nombre AS nombre_pelicula,
+      p.poster,
+      p.foto,
+      p.comentario
+    FROM funciones f
+    INNER JOIN salas s ON s.id_sala = f.id_sala
+    INNER JOIN peliculas p ON p.id_pelicula = f.id_pelicula
+    INNER JOIN cines c ON c.id_cine = s.id_cine
+    WHERE f.estado = 1 AND  f.id_funcion = id ORDER BY f.id_funcion DESC;
 END$$
 DELIMITER ;
 
@@ -88,7 +150,6 @@ BEGIN
 	UPDATE funciones f SET estado=0 WHERE f.id_funcion= id;
 END$$
 DELIMITER ;
-
 
 DROP procedure IF EXISTS `SP_FUN_EDIT`;
 DELIMITER $$
@@ -120,5 +181,36 @@ BEGIN
 	  f.sabado = sabado,
 	  f.domingo = domingo
 	WHERE f.id_funcion= idFuncion;
+END$$
+DELIMITER ;
+
+
+CREATE TABLE peliculas (
+  id_pelicula int NOT NULL,
+  nombre varchar(90) NOT NULL,
+  comentario varchar(250),  
+  poster varchar(90),
+  foto varchar(90),
+  fecha_salida DATE,
+  duracion int,
+  CONSTRAINT PK_PELICULAS PRIMARY KEY (id_pelicula)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP procedure IF EXISTS `SP_PELI_ADD`;
+DELIMITER $$
+CREATE PROCEDURE SP_PELI_ADD(IN  idPelicula int,
+  nombre varchar(90),
+  comentario varchar(250),
+  poster varchar(90),
+  foto varchar(90),
+  fechaSalida DATE,
+  duracion int)
+BEGIN
+	INSERT INTO peliculas
+    (peliculas.id_pelicula, peliculas.nombre, peliculas.comentario, peliculas.poster, peliculas.foto ,peliculas.fecha_salida, peliculas.duracion)
+VALUES
+    (idPelicula, nombre, comentario, poster, foto ,fechaSalida, duracion);
+    
 END$$
 DELIMITER ;
